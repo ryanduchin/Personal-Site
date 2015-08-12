1e759d0c9818fc39b6956604b0b31c042ad22b88
Ryan Duchin <ryanduchin@gmail.com>
2015-08-12 09:58:42 -0700

    app.rb settings

--- Personal-Site/config/application.rb
+++ Personal-Site/config/application.rb
@@ -23,7 +23,7 @@ module PersonalSite
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.action_mailer.delivery_method = :sendmail
+    config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
 
