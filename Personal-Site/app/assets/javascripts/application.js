a705f8dd0eee80bec1039d94ee30a0963f3a1e48
Ryan Duchin <ryanduchin@gmail.com>
2015-08-11 18:48:27 -0700

    added font awesome

--- Personal-Site/app/assets/javascripts/application.js
+++ Personal-Site/app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery.min
-//= require bootstrap
 //= require skel.min
 //= require skel-viewport.min
 //= require turbolinks
--- Personal-Site/config/application.rb
+++ Personal-Site/config/application.rb
@@ -26,5 +26,6 @@ module PersonalSite
     config.action_mailer.delivery_method = :sendmail
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
+
   end
 end
