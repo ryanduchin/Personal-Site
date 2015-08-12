8c6f4775d17c84d0974fc57ecb4fdd3a889243c3
Ryan Duchin <ryanduchin@gmail.com>
2015-08-12 09:27:58 -0700

    trying sendgrid with heroku domain

--- Personal-Site/config/environment.rb
+++ Personal-Site/config/environment.rb
@@ -2,9 +2,9 @@
 require File.expand_path('../application', __FILE__)
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["SENDGRID_USERNAME"],
-  :password => ENV["SENDGRID_PASSWORD"],
-  :domain => 'ryanduchin.com',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
