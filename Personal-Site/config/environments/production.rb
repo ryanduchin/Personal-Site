de03bb8072532cc4c564d059ba38dd34023ee1cd
Ryan Duchin <ryanduchin@gmail.com>
2015-08-12 00:47:35 -0700

    precompile and add compile line to production

--- Personal-Site/config/environments/production.rb
+++ Personal-Site/config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+  # to compile background image
+  config.assets.compile = true
+
 end
