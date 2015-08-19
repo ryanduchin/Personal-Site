bbbb5b1003bcec4d4498ec0edb7cf457de80a758
Ryan Duchin <ryanduchin@gmail.com>
2015-08-19 16:54:41 -0700

    edited icon animation. fixing rendering of messages on sent mail. fixed font issue by adding to html in previous git push.

--- Personal-Site/app/controllers/contact_controller.rb
+++ Personal-Site/app/controllers/contact_controller.rb
@@ -7,12 +7,14 @@ class ContactController < ApplicationController
       if valid_email(email)
         UserMailer.contact_email(name, email, subject, message).deliver
         UserMailer.confirm_email(name, email, subject, message).deliver
-        # flash.now.notice = "Message Sent!"
-        redirect_to root_url, notice: "Message Sent!"
+        flash.notice = "Message Sent!"
+        # flash[:notice] = "Message sent!"
+        render "static_pages/index"
       else
-        # flash.now.alert = "Error: Invalid email!"
-        flash.keep
-        redirect_to root_url(anchor: 'contact'), notice: "Error invalid email"
+        flash.alert = "Error: Invalid email!"
+        # flash.keep
+        # redirect_to root_url(anchor: 'contact'), notice: "Error invalid email"
+        render "static_pages/index#contact"
       end
   end
 
