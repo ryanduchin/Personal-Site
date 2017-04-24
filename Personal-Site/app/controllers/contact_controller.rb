6a03b14436aa277c656078b6f6bdde7be0825972
Ryan Duchin <ryanduchin@gmail.com>
2017-04-24 13:44:26 -0700

    fixed flash alerts and persistence of flash notices

--- Personal-Site/app/controllers/contact_controller.rb
+++ Personal-Site/app/controllers/contact_controller.rb
@@ -1,26 +1,28 @@
 class ContactController < ApplicationController
   def send_mail
-    name = params[:name]
-    email = params[:email]
-    subject = params[:subject]
-    message = params[:message]
-    if valid_email_address?(email)
-      UserMailer.confirm_email(name, email, subject, message).deliver
-      if email_parameters_are_present?(name, email, subject, message) #stricter logic to reduce spam
-        UserMailer.contact_email(name, email, subject, message).deliver
+    @name = params[:name]
+    @email = params[:email]
+    @subject = params[:subject]
+    @message = params[:message]
+    if valid_email_address?
+      UserMailer.confirm_email(@name, @email, @subject, @message).deliver
+      if email_parameters_are_present? #stricter logic to reduce spam
+        UserMailer.contact_email(@name, @email, @subject, @message).deliver
       end
+      flash.notice = "Thank you for your message!"
+    else
+      flash.alert = "Error: invalid email"
     end
-    flash.notice = "Thank you for your message!"
     render "static_pages/index"
   end
 
   private
 
-  def valid_email_address?(email)
-    (/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ =~ email) != nil
+  def valid_email_address?
+    (/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ =~ @email) != nil
   end
 
-  def email_parameters_are_present?(name, email, subject, message)
-    [name, email, subject, message].map(&:present?).all?
+  def email_parameters_are_present?
+    [@name, @email, @subject, @message].map(&:present?).all?
   end
 end
