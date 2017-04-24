cfec4545d6aad331ab588d1346cc07c4a9d8d763
Ryan Duchin <ryanduchin@gmail.com>
2017-04-24 12:54:01 -0700

    change ContactController logic to fight spam

--- Personal-Site/app/controllers/contact_controller.rb
+++ Personal-Site/app/controllers/contact_controller.rb
@@ -1,24 +1,26 @@
 class ContactController < ApplicationController
   def send_mail
-      name = params[:name]
-      email = params[:email]
-      subject = params[:subject]
-      message = params[:message]
-      if valid_email(email)
+    name = params[:name]
+    email = params[:email]
+    subject = params[:subject]
+    message = params[:message]
+    if valid_email_address?(email)
+      UserMailer.confirm_email(name, email, subject, message).deliver
+      if email_parameters_are_present?(name, email, subject, message) #stricter logic to reduce spam
         UserMailer.contact_email(name, email, subject, message).deliver
-        UserMailer.confirm_email(name, email, subject, message).deliver
-        flash.alert = "Message Sent!"
-        render "static_pages/index"
-      else
-        flash.alert = "Error: Invalid email!"
-        render "static_pages/index"
       end
+    end
+    flash.notice = "Thank you for your message!"
+    render "static_pages/index"
   end
 
   private
 
-  def valid_email(email)
+  def valid_email_address?(email)
     (/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ =~ email) != nil
   end
 
+  def email_parameters_are_present?(name, email, subject, message)
+    [name, email, subject, message].map(&:present?).all?
+  end
 end
